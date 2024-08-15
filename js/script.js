bdb542b7ff732d6e2aa104f645e2631b4ce1a562
shakil218 <rabiulhasanshakil218@gmail.com>
2024-08-15 11:05:48 +0600

    some issues solved

--- js/script.js
+++ js/script.js
@@ -10,11 +10,11 @@ function getAlbum(photos){
     const photosDiv = document.createElement('div')
     photosDiv.classList.add('photos')
     photosDiv.innerHTML = `
-      <h6>AlbumId:${photo.albumId}</h6>
-      <h6>Id:${photo.id}</h6>
-      <h5>Title:${photo.title}</h5>
-      <h6>Title:${photo.url}</h6>
-      <h6>Thumbnail-Url:${photo.thumbnailUrl}</h6>
+      <h6>AlbumId: ${photo.albumId}</h6>
+      <h6>Id: ${photo.id}</h6>
+      <h5>Title: ${photo.title}</h5>
+      <h6>Url: ${photo.url}</h6>
+      <h6>Thumbnail-Url: ${photo.thumbnailUrl}</h6>
     `
     photosContainer.appendChild(photosDiv)
   }
